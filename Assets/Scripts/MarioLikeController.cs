068e89c12f58086fcb2c39784130f4a7f3e23448
adesima <simaadesebastian@gmail.com>
2025-11-16 13:39:01 +0200

    stop code

--- Assets/Scripts/MarioLikeController.cs
+++ Assets/Scripts/MarioLikeController.cs
@@ -1,39 +1,68 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MarioLikeController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Animator animator;
-
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float speed;
     [SerializeField] private float jumpSpeed;
-    public Vector3 playerMoveDirection;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
-        playerMoveDirection = new Vector3(inputX, inputY).normalized;
-
-        animator.SetFloat("moveX", inputX);
-        animator.SetFloat("moveY", inputY);
-
-        if (playerMoveDirection == Vector3.zero)
-            animator.SetBool("moving", false);
-        else
-            animator.SetBool("moving", true);
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(playerMoveDirection.x * moveSpeed * Time.deltaTime,
-            playerMoveDirection.y * jumpSpeed * Time.deltaTime);
+        rb.linearVelocity = new Vector3(Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime, rb.linearVelocity.y);
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y) + Vector2.down * 0.3f, Vector2.down * 0.5f);
+        if (Input.GetKeyDown(KeyCode.Space))
+            if(Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y) + Vector2.down * 0.3f, Vector2.down, 0.5f))
+                rb.AddForce(Vector2.up * jumpSpeed * Time.fixedDeltaTime);
     }
 }
+
+
+//using UnityEngine;
+
+//public class MarioLikeController : MonoBehaviour
+//{
+//    public float moveSpeed = 5f;
+//    public float jumpForce = 7f;
+//    public Transform groundCheck;
+//    public float checkRadius = 0.2f;
+//    public LayerMask groundLayer;
+
+//    private Rigidbody2D rb;
+//    private bool isGrounded;
+//    private float moveInput;
+//    private SpriteRenderer sr;
+
+//    void Start()
+//    {
+//        rb = GetComponent<Rigidbody2D>();
+//        sr = GetComponent<SpriteRenderer>();
+//    }
+
+//    void Update()
+//    {
+//        // Mișcare stânga-dreapta
+//        moveInput = Input.GetAxisRaw("Horizontal");
+//        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+//        // întoarcerea sprite-ului după direcție
+//        if (moveInput > 0) sr.flipX = false;
+//        if (moveInput < 0) sr.flipX = true;
+
+//        // Sărit
+//        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+//        if (Input.GetButtonDown("Jump") && isGrounded)
+//        {
+//            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+//        }
+//    }
+//}
